4beef2e1762e36e13968bfd8f4647483eef7c529
Kaylyn <klattk@gmail.com>
2018-06-03 17:47:10 -0400

    replaced scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,102 @@
 (function() {
 
-var modalLinks = document.getElementsByClassName('modal-link');
-for (var i = 0; i < modalLinks.length; i++) {
-modalLinks[i].addEventListener('click', function(evt){
-  evt.preventDefault();
+  var modalLinks = document.getElementsByClassName('modal-link');
+  for (var i = 0; i < modalLinks.length; i++) {
+    modalLinks[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
 
+      // Find the modal link's data attribute value
+      var modalId = this.getAttribute('data-modal');
 
+      // Find the DOM element with that same ID
+      var modal = document.getElementById(modalId);
 
-var modalId = this.getAttribute('data-modal');
+      // Add the 'open' class to that DOM element
+      modal.classList.add('open');
 
-var modal = document.getElementById(modalId);
-modal.classList.add('open');
-
-
-
-
-});
-
-}
-
-var modalClose = document.getElementsByClassName('modal-content__close');
-
-for (var i = 0; i < modalClose.length; i++) {
-modalClose[i].addEventListener('click', function() {
-  var openModal = document.querySelector('.open');
-openModal.classList.remove('open');
-});
-}
-
-
-//add event listener to each filter
-var filter = document.getElementsByClassName('portfolio-filter');
-for (var i = 0; i < filter.length; i++) {
-  filter.addEventlistener('click', function (evt){
-   var filterId = this.getAttribute('filter');
-   var project = document.getElementById(filterId);
-   project.classList.add('active');
-
-
-
-});
-}
-
-var webDevInput = document.getElementById('filter1');
-var projects = document.getElementsByClassName('project');
-webDevInput.addEventListener('change', function() {
-console.log('changed!');
-  for (var k = 0; k <projects.length; k++) {
-    //does the porject have a data-catagriy === '1'
-
-    var projectCatagory = projects[k].getAttribute('data-catagory');
-      console.log(projectCatagory);
-       if (projectCatagory === '1') {
-      projects[k].classList.add('active');
-      }
-      else {
-    projects[k].classList.remove('active');
-      }
+    });
   }
 
-});
-//if project doesn't have catagory remove active classList
+  // Find all close button elements
+  var closeButtons = document.getElementsByClassName('modal-content__close');
 
-//if project does have catagory add active class
+  // Attach an event listener that removes the 'open' class from
+  // a open modal element
+  for (var j = 0; j < closeButtons.length; j++) {
+    closeButtons[j].addEventListener('click', function() {
+      var openModal = document.querySelector('.open');
+      openModal.classList.remove('open');
+    });
+  }
 
+  /* FILTERING SOLUTION 1:
+   * This was the solution we did in class, where you add
+   * an event listener to each input and filter based on that.
+   * The downside is that you'd need to do this for every
+   * single input, which creates a lot of code! We could make
+   * a function out of it to reduce the amount of code.
+   */
+  // // Get an individual input
+  // var webDevInput = document.getElementById('filter1');
+  // Get all project inputs
+  // var projects = document.getElementsByClassName('project');
+  // webDevInput.addEventListener('change', function() {
+  //   console.log('changed!');
+  //   for (var k = 0; k < projects.length; k++) {
+  //     // Does the project have a data-category === '1'
 
+  //     var projectCategory = projects[k].getAttribute('data-category');
+  //     console.log(projectCategory);
+  //     if (projectCategory === '1') {
+  //       // It matches! Let's activate
+  //       projects[k].classList.add('active');
+  //     } else {
+  //       // No match -- remove the class!
+  //       projects[k].classList.remove('active');
+  //     }
+  //   }
+  // });
 
+   /* FILTERING SOLUTION 2:
+    * This solution allows us to loop through a set of filters and projects
+    * by class name, which is more flexible. We'll make use of a closure
+    * to fix a scoping issue.
+    */
+  // Get all filter input elements
+  var filters = document.getElementsByClassName('portfolio-filter');
+  // Get all project inputs
+  var projects = document.getElementsByClassName('project');
+
+  // Loop through each filter to add an event listener
+  for (var k = 0; k < filters.length; k++) {
+
+    // Use a closure to pass the value of k into the callback as filterIndex
+    (function(filterIndex) {
+
+      filters[filterIndex].addEventListener('change', function() {
+
+        // When a filter changes, loop through projects
+        for (var m = 0; m < projects.length; m++) {
+
+          // Get the category from each project
+          var projectCategory = projects[m].getAttribute('data-category');
+
+          if (filterIndex === 0) {
+            // Someone clicked 'All', so every project should be active
+            projects[m].classList.add('active');
+
+          } else if (projectCategory === filterIndex.toString()) {
+            // It matches! Let's activate. Note that we convert m to a
+            // string so we can use ===, which tests if the two
+            // variables are equal in value AND type
+            projects[m].classList.add('active');
+
+          } else {
+            // No match -- remove the class!
+            projects[m].classList.remove('active');
+          }
+        }
+      });
+    })(k);
+  }
 })();
