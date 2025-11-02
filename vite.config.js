75df5ee43d7a80e2f47f3ca9c1c147626045e808
kaydc18 <klattk@gmail.com>
2025-11-02 10:22:19 -0500

    try to get deploy to work

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kaydc18.github.io/',
   plugins: [
     react(),
     tailwindcss()
